45293e0752f068014cd8b81705531d05dcf4c00e
d22isaka <d22isaka@student.his.se>
2023-05-04 23:50:26 +0200

    Created an about-activity which use the webview widget. Created a button in MainActivity with Intent to start About activity if clicked.
    Made MainActivity to parent for About-Activity.

---
+++ app/src/main/java/com/example/project/About.java
@@ -0,0 +1,32 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class About extends AppCompatActivity {
+
+    WebView myWebView;
+
+    public void showInternalWebPage(){
+
+        myWebView.loadUrl("file:///android_asset/about.html");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        myWebView = findViewById(R.id.my_webview);
+        myWebView.setWebViewClient(new WebViewClient()); // Do not open in Chrome!
+
+        WebView myWebView = (WebView) findViewById(R.id.my_webview);
+        WebSettings webSettings = myWebView.getSettings();
+
+        showInternalWebPage();
+    }
+}
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button about = findViewById(R.id.about);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, About.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
